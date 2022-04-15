941a435cc44aeb0dfeff336212c4cb33ea02d14a
xristremains <harangi.peter12@gmail.com>
2022-04-15 15:21:33 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+  import LandingPage from './components/LandingPage.vue';
+  import AppFooter from './components/AppFooter.vue'
+</script>
+
+<template>
+  <main>
+    <LandingPage/>
+  </main>
+  <AppFooter/>
+</template>
+
+<style>
+@import './assets/base.css';
+@import url('https://fonts.googleapis.com/css2?family=Nunito&display=swap');
+</style>
